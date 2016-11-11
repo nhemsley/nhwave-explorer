37c2bcf5a94463196fb07a676461aa61242ef476
Nicholas Hemsley <nick.hems@gmail.com>
2016-11-12 07:40:14 +0800

    stuff

--- depth-to-obj.rb
+++ depth-to-obj.rb
@@ -24,9 +24,12 @@ y=0
 File.foreach(input_file) do |line|
   line_arr = line.split(' ')
 
-  points << line_arr.map.with_index(0) do |p, x|
-    [x.to_f, -p.to_f * 100, y.to_f ]
+  points << line_arr.map.with_index(0) do |depth, x|
+    [x.to_f, depth.to_f, y.to_f ]
   end
+
+  min = [min, row_mapped.min_by{|d| d.depth}].min
+  max = [max, row_mapped.max_by{|d| d.depth}].max
 
   # puts point_arr.inspect
   y = y + 1
